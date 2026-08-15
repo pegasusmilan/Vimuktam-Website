dd8decf43d183c32153dbdba51dc7ef939f62333
pegasusmilan <pegasus.milan@gmail.com>
2026-08-16 05:20:18 +0530

    Serve homepage media from R2

--- worker.js
+++ worker.js
@@ -24,6 +24,17 @@ export default {
       }
     }
 
+    if (url.pathname === "/media/Hindi-teacher-Milan.webp") {
+      const object = await env.MULTIMEDIA.get("Media/images/Hindi-teacher-Milan.webp");
+      if (!object) return new Response("Not found", { status: 404 });
+
+      const headers = new Headers();
+      object.writeHttpMetadata(headers);
+      headers.set("etag", object.httpEtag);
+      headers.set("cache-control", "public, max-age=3600");
+      return new Response(object.body, { headers });
+    }
+
     return env.ASSETS.fetch(request);
   },
 };
